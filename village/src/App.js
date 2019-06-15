e45a4e184281c4a29fde05ad702ab0ab9cda6d68
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-06-14 20:15:55 -0500

    Created Post to add smurf.

--- village/src/App.js
+++ village/src/App.js
@@ -21,6 +21,16 @@ class App extends Component {
     })
     .catch(err=>console.log(err))
   }
+
+  addSmurf = (data) => {
+    axios.post('http://localhost:3333/smurfs', data)
+    .then(response => {console.log(response)
+      this.setState({ smurfs: response.data })
+    })
+    .catch(err => console.log(err))
+  }
+  
+  
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
