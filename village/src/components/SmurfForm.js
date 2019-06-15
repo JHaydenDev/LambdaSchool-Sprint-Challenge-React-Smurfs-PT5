22aa5e88890d12058b4d14f678e3daeee1be300d
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-06-14 20:21:23 -0500

    add smurf working

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     };
   }
 
   addSmurf = event => {
-    event.preventDefault();
-    // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
+    this.postSmurf({
+      name: `${this.state.name}`,
+      age: `${this.state.age}`,
+      height: `${this.state.height}`
     });
-  }
+  };
+  postSmurf = smurf => {
+    axios
+      .post("http://localhost:3333/smurfs", smurf)
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
